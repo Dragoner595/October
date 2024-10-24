09544cee00c0eb4a98b84e3af3f811fd0722d21a
Dmitry <122685309+Dragoner595@users.noreply.github.com>
2024-10-24 08:55:38 +0000

    modified:   video_loading.py
    commit

--- video_loading.py
+++ video_loading.py
@@ -109,3 +109,62 @@ def dowload_from_zip(zip_url,to_dir,file_names):
       output_file = to_dir / class_name / fn
       unzipped_file.rename(output_file)
 
+def split_class_lists(files_for_class, count):
+  """ Returns the list of files belonging to a subset of data as well as the remainder of
+    files that need to be downloaded.
+
+    Args:
+      files_for_class: Files belonging to a particular class of data.
+      count: Number of files to download.
+
+    Returns:
+      Files belonging to the subset of data and dictionary of the remainder of files that need to be downloaded.
+  """
+  split_files = []
+  remainder = {}
+  for cls in files_for_class:
+    split_files.extend(files_for_class[cls][:count])
+    remainder[cls] = files_for_class[cls][count:]
+  return split_files, remainder
+
+def dowload_ucf_101_subset(zip_url,num_classes,splits,dowload_dir):
+  """ Download a subset of the UCF101 dataset and split them into various parts, such as
+    training, validation, and test.
+
+    Args:
+      zip_url: A URL with a ZIP file with the data.
+      num_classes: Number of labels.
+      splits: Dictionary specifying the training, validation, test, etc. (key) division of data
+              (value is number of files per split).
+      download_dir: Directory to download data to.
+
+    Return:
+      Mapping of the directories containing the subsections of data.
+  """
+  files = list_files_from_zip_url(zip_url)
+  for f in files:
+    path = os.path.normpath(f)
+    tokens = path.split(os.sep)
+    if len(tokens) <= 2 :
+      files.remove(f) # Remove that item from the list if it does not have a filename
+
+  files_for_class = get_files_per_class(files)
+
+  classes = list(files_for_class.keys())[:num_classes]
+
+  for cls in classes:
+    random.shuffle(files_for_class[cls])
+
+  # Only use the number of classes you want in the dictionary
+
+  files_for_class = { x: files_for_class[x] for x in classes}
+
+  dirs = {}
+  for split_name , split_count in splits.items():
+    print(split_name, ":")
+    split_dir = download_dir / split_name
+    splits_files , files_for_class = split_class_list(files_for_class, split_count)
+    dowload_from_zip(zip_url, split_dir,split_files)
+    dirs[split_name] = split_dir
+
+  return dirs
